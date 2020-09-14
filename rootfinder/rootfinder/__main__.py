5581d3ea970e8ca2ecbb83c14bc9b95f887f05a8
JakeBoord <boord01@allegheny.edu>
2020-09-14 15:34:39 -0400

    Update __main__.py
    Began to debug the incorrect features of the code.

--- rootfinder/rootfinder/__main__.py
+++ rootfinder/rootfinder/__main__.py
@@ -6,15 +6,16 @@ from rootfinder import rootfind
 
 
 def main(
-    a: float = typer.Option(1), b: float = typer.Option(2), c: float = typer.Option(2)
+    a: float = typer.Option(1), b: float = typer.Option(2), c: float = typer.Option(3)
 ):
     """Calculate the roots of a quadratic equation using the quadratic formula."""
     # TODO: display the debugging output for the program's command-line arguments
     typer.echo(f"Calculating the reals of a quadratic equation with:")
     typer.echo(f"   a = {a}")
     typer.echo(f"   b = {b}")
+    typer.echo(f"   c = {c}")
     # TODO: perform the calculation of the roots for the quadratic equation
-    x_one, x_one = rootfind.calculate_quadratic_equation_roots(c, b, a)
+    x_one, x_two = rootfind.calculate_quadratic_equation_roots(a, b, c)
     # TODO: output the values from running the calculation of the quadratic
     # equation's roots with the calculate_quadratic_equation_roots function
     typer.echo(f"   x_one = {x_one}")
