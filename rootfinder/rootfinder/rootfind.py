957b99bced659f65ae0c1ae1adc630e0c67e4ea3
JakeBoord <boord01@allegheny.edu>
2020-09-15 12:54:03 -0400

    Update rootfind.py
    Corrected the equation variables

--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -6,6 +6,6 @@
 def calculate_quadratic_equation_roots(a: float, b: float, c: float):
     """Calculate the roots of a quadratic equation."""
     D = (b * b - 4 * a * c) ** 0.5
-    x_one = (-b + 2) / (2 * a)
-    x_two = (-b - 2) / (2 * a)
+    x_one = (-b + D) / (2 * a)
+    x_two = (-b - D) / (2 * a)
     return x_one, x_two
