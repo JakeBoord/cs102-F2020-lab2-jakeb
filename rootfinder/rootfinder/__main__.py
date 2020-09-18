874c71cfde8345fa64369f62a418ab4e75343794
JakeBoord <boord01@allegheny.edu>
2020-09-18 16:48:19 -0400

    Souce Code Update
    Made corrections to the source code

--- rootfinder/rootfinder/__main__.py
+++ rootfinder/rootfinder/__main__.py
@@ -9,14 +9,14 @@ def main(
     a: float = typer.Option(1), b: float = typer.Option(2), c: float = typer.Option(3)
 ):
     """Calculate the roots of a quadratic equation using the quadratic formula."""
-    # TODO: display the debugging output for the program's command-line arguments
+    # display the debugging output for the program's command-line arguments
     typer.echo(f"Calculating the reals of a quadratic equation with:")
     typer.echo(f"   a = {a}")
     typer.echo(f"   b = {b}")
     typer.echo(f"   c = {c}")
-    # TODO: perform the calculation of the roots for the quadratic equation
+    # perform the calculation of the roots for the quadratic equation
     x_one, x_two = rootfind.calculate_quadratic_equation_roots(a, b, c)
-    # TODO: output the values from running the calculation of the quadratic
+    # output the values from running the calculation of the quadratic
     # equation's roots with the calculate_quadratic_equation_roots function
     typer.echo(f"   x_one = {x_one}")
     typer.echo(f"   x_two = {x_two}")
--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -1,6 +1,6 @@
 """Calculate the roots of a quadratic equation."""
 
-# TODO: Repair any defects in the function provided by this module
+
 
 
 def calculate_quadratic_equation_roots(a: float, b: float, c: float):
