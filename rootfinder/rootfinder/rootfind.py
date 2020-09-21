7b80defb4201bd3970680324ac14712427e22a7f
JakeBoord <boord01@allegheny.edu>
2020-09-21 13:37:04 -0400

    Update rootfind.py
    Changed some things around to try and fix my error

--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -2,8 +2,8 @@
 
 
 def calculate_quadratic_equation_roots(a: float, b: float, c: float):
-    # calculate the roots of a quadratic equation.
-    D = (b * b - 4 * a * c) ** 0.5
+    """Calculate the roots of a quadratic equation"""
+    D = ((b * b) - (4 * a * c)) ** 0.5
     x_one = (-b + D) / (2 * a)
     x_two = (-b - D) / (2 * a)
     return x_one, x_two
