5ed72fec5ccc3cf993cd81eb055710b7ea0bba8f
JakeBoord <boord01@allegheny.edu>
2020-09-21 13:26:18 -0400

    Update rootfind.py
    fixed capitalization issue

--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -2,7 +2,7 @@
 
 
 def calculate_quadratic_equation_roots(a: float, b: float, c: float):
-    # Calculate the roots of a quadratic equation.
+    # calculate the roots of a quadratic equation.
     D = (b * b - 4 * a * c) ** 0.5
     x_one = (-b + D) / (2 * a)
     x_two = (-b - D) / (2 * a)
